bdfb0c65d7740b0a1661a02d6c514e48efa3fc97
Jakub Fabia <jakub.fabia@proton.me>
2025-06-15 22:44:41 +0200

    CORS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,24 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [hotels, setHotels] = useState([])
 
-  return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    useEffect(() => {
+        fetch('http://localhost:8080/api/hotels/1')
+            .then(res => res.json())
+            .then(setHotels)
+    }, [])
+
+    return (
+        <div>
+            <h1>Lista hoteli</h1>
+            <ul>
+                {hotels.map(h => (
+                    <li key={h.id}>{h.name}</li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 export default App
